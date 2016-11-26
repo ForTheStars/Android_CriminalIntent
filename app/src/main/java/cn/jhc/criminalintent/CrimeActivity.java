e72bf0309c6e6b00753b47045695bd7314a9db5b
王建源 <971800495@qq.com>
2016-11-26 15:50:50 +0800

    alter fragment

--- app/src/main/java/cn/jhc/criminalintent/CrimeActivity.java
+++ app/src/main/java/cn/jhc/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package cn.jhc.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,6 +11,16 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
     }
 
 }
