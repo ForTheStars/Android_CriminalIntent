7e102e05da43075fb023205af6379fbe5456b904
王建源 <971800495@qq.com>
2016-12-07 23:35:54 +0800

    DataPicker

--- CriminalIntent/app/src/main/java/cn/jhc/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/cn/jhc/criminalintent/CrimeFragment.java
@@ -1,8 +1,12 @@
 package cn.jhc.criminalintent;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -13,22 +17,41 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
+import java.util.UUID;
+
 import cn.jhc.criminalintent.bean.Crime;
+import cn.jhc.criminalintent.db.CrimeLab;
+import cn.jhc.criminalintent.util.LogUtils;
 import cn.jhc.criminalintent.util.TimeUtil;
 
 /**
  * Created by Administrator on 2016-11-25.
  */
 public class CrimeFragment extends Fragment {
+    private static final String AGR_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    public static CrimeFragment newIntent(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(AGR_CRIME_ID,crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID) getArguments().getSerializable(AGR_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        returnResult();
     }
 
     @Nullable
@@ -37,6 +60,7 @@ public class CrimeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_crime,container,false);
 
         mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getmTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -56,10 +80,21 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) view.findViewById(R.id.crime_date);
-        mDateButton.setText(TimeUtil.DateFormat(mCrime.getmDate()));
-        mDateButton.setEnabled(false);
+        updateDate();
+//        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getFragmentManager();
+               // DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getmDate());
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);
+                dialog.show(manager,DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.ismSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -68,4 +103,29 @@ public class CrimeFragment extends Fragment {
         });
         return view;
     }
+
+    public void returnResult() {
+        Intent intent = new Intent();
+        intent.putExtra("mcrime_id",mCrime.getmTitle());
+        getActivity().setResult(Activity.RESULT_OK,intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if(requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setmDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(TimeUtil.DateFormat(mCrime.getmDate()));
+    }
+
+
 }
--- CriminalIntent/app/src/main/java/cn/jhc/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/cn/jhc/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package cn.jhc.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,16 +14,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.UUID;
 
 import cn.jhc.criminalintent.bean.Crime;
 import cn.jhc.criminalintent.db.CrimeLab;
+import cn.jhc.criminalintent.util.LogUtils;
 
 /**
  * Created by Administrator on 2016-11-27.
  */
 public class CrimeListFragment extends Fragment{
+
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private static final int REQUEST_CRIME = 1;
 
     @Nullable
     @Override
@@ -34,12 +39,21 @@ public class CrimeListFragment extends Fragment{
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener
@@ -66,7 +80,18 @@ public class CrimeListFragment extends Fragment{
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),mCrime.getmTitle() + "clicked!", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getActivity(),mCrime.getmTitle() + "clicked!", Toast.LENGTH_SHORT).show();
+            Intent intent = CrimePagerActivity.newIntent(getActivity(),mCrime.getmId());
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CRIME) {
+            String title = (String) data.getExtras().get("mcrime_id");
+            LogUtils.e(LogUtils.LOG_TAG,title);
         }
     }
 
