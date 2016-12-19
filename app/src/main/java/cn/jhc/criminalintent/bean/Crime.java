3d9fdcd7a5e20720196637b1e561a918782dc238
王建源 <971800495@qq.com>
2016-12-19 21:28:30 +0800

    add suspect

--- app/src/main/java/cn/jhc/criminalintent/bean/Crime.java
+++ app/src/main/java/cn/jhc/criminalintent/bean/Crime.java
@@ -11,6 +11,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
         this(UUID.randomUUID());
@@ -47,6 +48,14 @@ public class Crime {
 
     public void setmSolved(boolean mSolved) {
         this.mSolved = mSolved;
+    }
+
+    public String getmSuspect() {
+        return mSuspect;
+    }
+
+    public void setmSuspect(String mSuspect) {
+        this.mSuspect = mSuspect;
     }
 
     @Override
